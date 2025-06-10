967e88b7b994ab33530c0d48ce8e2504c43c7653
Lutik72 <gribova.sofia24@yandex.ru>
2025-06-10 20:52:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ function showAlert() { alert("Отличный выбор, " + Name); }
 document.addEventListener("DOMContentLoaded", function() {
 console.log("страница загрузилась");
 });
+
+    let btn1 = document.getElementById('1');
+    let btn2 = document.getElementById('2');
+    let btn3 = document.getElementById('3');
+    btn1.addEventListener('click', function()
+    { console.log('Выбран капучино');
+    }); 
+    btn2.addEventListener('click', function() 
+    { console.log('Выбран латте'); 
+    }); 
+    btn3.addEventListener('click', function() 
+    { console.log('Выбран горячий шоколад');
+    });
