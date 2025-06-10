078cbd6d6439566a0fbb564fa3499a9cf1003aa6
Lutik72 <gribova.sofia24@yandex.ru>
2025-06-10 18:30:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 let Name = prompt("Добро пожаловать! Как вас зовут?");
 alert("Приятно познакомиться, " + Name)
 function showAlert() { alert("Отличный выбор, " + Name); } 
+
+document.addEventListener("DOMContentLoaded", function() {
+console.log("страница загрузилась");
+});
