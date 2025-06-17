f0e60110e27b29c821c17765aaeb0af42d15a320
Lutik72 <gribova.sofia24@yandex.ru>
2025-06-17 11:26:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-let Name = prompt("Добро пожаловать! Как вас зовут?");
-alert("Приятно познакомиться, " + Name)
-function showAlert() { alert("Отличный выбор, " + Name); } 
+alert("Добро пожаловать!");
 
-document.addEventListener("DOMContentLoaded", function() {
-console.log("страница загрузилась");
-});
-
-    let btn1 = document.getElementById('1');
-    let btn2 = document.getElementById('2');
-    let btn3 = document.getElementById('3');
-    btn1.addEventListener('click', function()
-    { console.log('Выбран капучино');
-    }); 
-    btn2.addEventListener('click', function() 
-    { console.log('Выбран латте'); 
-    }); 
-    btn3.addEventListener('click', function() 
-    { console.log('Выбран горячий шоколад');
-    });
+function showAlert() { alert("Отличный выбор!"); } 
