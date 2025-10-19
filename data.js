d44fc729ab5b923a1e99fb22bed6ea63592cd840
Sofia <gribova.sofia24@yandex.ru>
2025-10-19 19:02:24 +0300

    обновление файлов

---
+++ data.js
@@ -0,0 +1,14 @@
+dataProducts = {
+"title": "MANGO PEOPLE T-SHIRT",
+"img": "Shop/img/item3.png",
+"price": "Price: $300",
+"color": "Color: Red",
+"size": "Size: XL"
+},
+{
+"title": "ELLERY X M`O CAPSULE",
+"img": "Shop/img/item2.png",
+"price": "Price: $52",
+"color": "Color: Black",
+"size": "Size: M" 
+}
